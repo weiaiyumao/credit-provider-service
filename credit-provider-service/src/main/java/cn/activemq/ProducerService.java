9cd37346d3ce4fda242f9e633f5fdbf44ed18b8a
empty <weiaiyumao@163.com>
2017-11-13 16:15:32 +0800

    新增配置

--- credit-provider-service/src/main/java/cn/activemq/ProducerService.java
+++ credit-provider-service/src/main/java/cn/activemq/ProducerService.java
@@ -21,7 +21,7 @@ public class ProducerService {
 	public void sendMessage(Destination destination, final String message) {
 		jmsTemplate.convertAndSend(destination, message);
 	}
-
+	
 	@JmsListener(destination = "out.queue")
 	public void consumerMessage(String text) {
 		System.out.println("从out.queue队列收到的回复报文为:" + text);
